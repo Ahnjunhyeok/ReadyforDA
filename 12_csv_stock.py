13603b4cba14209693fe779b98792ccc36060365
Ahnjunhyeok <93477716+Ahnjunhyeok@users.noreply.github.com>
2022-02-25 23:30:29 +0900

    웹 스크래핑 파트 12 네이버 금융 버그 수정판
    링크를 그대로 복사 붙여넣기 하는 과정에서 page 뒤에 숫자가 붙어있었는데, 그 부분 때문에 원하는 정보를 불러오지 못했음. 해당 부분 수정 완료 후 코드 보완.

--- 12_csv_stock.py
+++ 12_csv_stock.py
@@ -2,9 +2,18 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page=1"
+url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page=" 
 
-for page in range(1, 2):
+filename = "시가총액1-200.csv"
+f = open(filename, "w", encoding="utf-8-sig", newline="")
+writer = csv.writer(f)
+
+title = "N	종목명	현재가	전일비	등락률	액면가	거래량	상장주식수	시가총액    외국인비율	PER	ROE".split("\t")
+# [N, "종목명", "현재가", ...]
+print(type(title))
+writer.writerow(title)
+
+for page in range(1, 5):
     res = requests.get(url + str(page))
     res.raise_for_status()
     soup = BeautifulSoup(res.text, "lxml")
@@ -15,4 +24,5 @@ for page in range(1, 2):
         if len(columns) <= 1: # 의미 없는 데이터는 skip 
             continue
         data = [column.get_text().strip() for column in columns]
-        print(data)
+        # print(data)
+        writer.writerow(data)
