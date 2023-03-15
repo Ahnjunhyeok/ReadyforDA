38d1cffc31533eedcad4fe3e4e3935f4c76a0d3d
안준혁(Ray) <93477716+Ahnjunhyeok@users.noreply.github.com>
2023-03-15 15:57:54 +0900

    R_BA_1
    R을 활용한 데이터분석 - 패키지 설치(RWeka)

---
+++ 2023-1/BA/R_BA_1.R
@@ -0,0 +1,18 @@
+# 비즈니스어낼리틱스 2주차 2차시
+
+# Machine learning with R page 58~
+
+# R 패키지 설치
+# 이용할 수 있는 R 애드온이 방대함에도 불구하고 패키지 형식은 설치와 사용을 매우 수월하게 해준다.
+# 패키지 사용을 설명하고자 RWeka 패키지를 설치하고 로드해보겠다. 
+# RWeka는 자바로 개발된 Weka 소프트웨어 패키지의 머신러닝 알고리즘을 R로 접근하도록 함수 모음을 제공한다.
+
+# 자바 설치법 및 설정 다루기 링크 
+# https://jhnyang.tistory.com/entry/%EC%9E%90%EB%B0%94JAVA-%EB%8B%A4%EC%9A%B4%EB%A1%9C%EB%93%9C-%EB%B0%8F-%EC%84%A4%EC%B9%98%ED%95%98%EA%B8%B0-%ED%99%98%EA%B2%BD%EC%84%A4%EC%A0%95-%EC%84%B8%ED%8C%85%ED%95%98%EB%8A%94-%EB%B2%95-jdk-vs-jre-java-SE-vs-java-EE-%EC%B0%A8%EC%9D%B4%EC%A0%90-jdk-13-%EC%84%A4%EC%B9%98
+# https://boan-boan.tistory.com/42
+
+# colab에서 R 사용하기
+# https://gunn.kim/post/2020-09-24-colab-support-r/
+
+# 패키지 설치
+install.packages("RWeka")
